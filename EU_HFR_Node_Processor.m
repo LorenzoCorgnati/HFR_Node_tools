37fc95db1c4553e7b2d48e8d592755f9aee016c5
LorenzoCorgnati <lorenzo.corgnati@sp.ismar.cnr.it>
2019-06-26 16:43:37 +0200

    Modification of the global attributes related to calibration

--- EU_HFR_Node_Processor.m
+++ EU_HFR_Node_Processor.m
@@ -56,7 +56,7 @@ while(kk>0)
     
     try
         startDate = startCombinationDate(now);
-%         startDate = '2012-01-29';
+        startDate = '2012-01-29';
     catch err
         disp(['[' datestr(now) '] - - ERROR in ' mfilename ' -> ' err.message]);
         HFRC_err = 1;
@@ -65,7 +65,7 @@ while(kk>0)
     %%
     
     % RADIALS COMBINATION & RADIALS AND TOTALS CONVERSION
-    inputRUV2DB;
+%     inputRUV2DB;
 %     inputCradAscii2DB;
     HFRCombiner;
     
